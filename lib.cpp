ffb7801d3cc13931b04c0ccd7dacc1cbf9bf1abb
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-12-12 09:45:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,8 @@
 #include "lib.h"
+
 float funzione(float numeri[], int dimensione){
     float massimo= numeri[0];
-    for (int i=0;i < dimensione ;i++) {
+    for (int i=0;i < dimensione;i++) {
         if (massimo < numeri[i]) {
             massimo = numeri[i];
         }
