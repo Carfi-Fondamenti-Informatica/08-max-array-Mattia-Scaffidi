bb2b7a1ac77443fda1ff4b3bc3a179cecaf4489e
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-12-12 09:42:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(){
     int dim;
-    int num[dim];
+    float num[dim];
     
     cin>>dim;
     for (int i=0;i< dim;i++){
         cin>> num[i];
     }
-    cout<< funzione(num, dim);
+    cout<< funzione(num, dim)<<endl;
 
     return 0;
 }
